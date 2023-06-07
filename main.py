f36ba5d60daded362a05dc748e60d5251d1f6d98
Aftermath001 <alvinasiachi@gmail.com>
2023-06-07 10:09:45 +0300

    Menu

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ class Patient(Base):
     lastName = Column(String())
     description = Column(String())
     level = Column(Integer())
-
     drivers = relationship('Driver', backref=backref('patient'))
-
+    # patients = relationship ('Patient', backref=backref('hospital'))
+    hospitals = relationship ('Hospital', backref=backref('patient'))
+    
     def __repr__(self):
         return f'Patient(id = {self.id})'+\
            f'firstName = {self.firstName}'+\
@@ -58,10 +59,14 @@ class Hospital(Base):
     id = Column(Integer(), primary_key= True)
     name = Column(String())
     location= Column(String())
+    # hospital_id = Column(Integer(), ForeignKey('hospital.id'))
+    patient_id = Column(Integer(), ForeignKey('patients.id'))
+    
     def __repr__ (self):
         return f' Hospital(id = {self.id})'+\
             f'name={self.name}'+\
-            f'location = {self.location}'
+            f'location = {self.location}'+\
+            f'patient_id = {self.patient_id}'
     
 if __name__ == '__main__':
     engine = create_engine('sqlite:///records.db')
@@ -96,35 +101,50 @@ if __name__ == '__main__':
         description = "Internal bleeding, body paleness",
         level = 5
         )
+    patient5 = Patient(
+        firstName ="Minayo",
+        lastName ="Victoria",
+        description = "Internal bleeding, body paleness",
+        level = 4
+        )
+    patient6 = Patient(
+        firstName ="Waka",
+        lastName ="Joy",
+        description = "Internal bleeding, body paleness",
+        level = 3
+        )
+    
     
     # Driver's Instances
     driver1 = Driver(
         name = "Teddy Makarios",
         patient_id = 1,
-        vehicle_id = 3
+        vehicle_id = 2
         )
     driver2 = Driver(
         name = "Thanos Imani",
-        patient_id = 1,
-        vehicle_id = 3
+        patient_id = 3,
+        vehicle_id = 1
         )
     driver3 = Driver(
         name = "Mkadin Ale",
-        patient_id = 1,
-        vehicle_id = 3
+        patient_id = 4,
+        vehicle_id = 5
         )
     driver4 = Driver(
         name = "James Kamau",
-        patient_id = 1,
-        vehicle_id = 3
+        patient_id = 5,
+        vehicle_id = 2
         )
     driver5 = Driver(
         name = "Mishy Dora",
-        patient_id = 1,
+        patient_id = 6,
         vehicle_id = 3
         )
     driver6 = Driver(
-        name = "Huddah Munroe"
+        name = "Huddah Munroe",
+        patient_id = 2,
+        vehicle_id = 4
         )
     
     # Vehicle Instances
@@ -168,3 +188,23 @@ if __name__ == '__main__':
     session.add_all([hospital1, hospital2,hospital3,hospital4])
 
     session.commit()
+# Menu details
+def main():
+
+    option = 0
+    while option != 4:
+        print("SELECT HOSPITAL")
+        print("1)Get a list of all hospitals")
+        print("2)Get the list of all drivers")
+        print("3)Choose an option here")
+        print("4)Leave Menu")
+        option = int(input())
+
+        if option == 1:
+        #   hospital_list =([])
+          hospital_list = session.query(Hospital).all()
+          hospital_list.append([])
+          print(hospital_list)
+
+if __name__ == "__main__":
+    main()
